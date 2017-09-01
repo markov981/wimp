6cb0f02c6c68959c2f9e5a6ca7b73ab7bc21afc9
Alex Karp <alex.karp@libertymutual.com>
2017-09-01 15:07:35 -0700

    GG

--- src/main/java/com/libertymutual/goforcode/wimp/api/AwardApiController.java
+++ src/main/java/com/libertymutual/goforcode/wimp/api/AwardApiController.java
@@ -2,23 +2,13 @@ package com.libertymutual.goforcode.wimp.api;
 
 import java.util.List;
 
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.libertymutual.goforcode.wimp.models.Actor;
-import com.libertymutual.goforcode.wimp.models.ActorWithMovies;
+
 import com.libertymutual.goforcode.wimp.models.Award;
-import com.libertymutual.goforcode.wimp.models.Movie;
-import com.libertymutual.goforcode.wimp.repositories.ActorRepository;
 import com.libertymutual.goforcode.wimp.repositories.AwardRepository;
-import com.libertymutual.goforcode.wimp.repositories.MovieRepository;
-import org.junit.Before;
+
 
 
 
